8b337e9519d46cb40a1f22fd68cd80eb783fbf9a
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-20 10:15:19 -0400

    Completed

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -7,7 +7,13 @@
 //
 
 
-class BankVault {
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
+
+
+
+class BankVault : ProvideAccess {
     
     let name: String
     let address: String
@@ -16,6 +22,21 @@ class BankVault {
     init(name: String, address: String) {
         self.name = name
         self.address = address
+    }
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        
+        if password.isEmpty || password.count > 10 || password[0] % 2 == 1 { return false }
+        var passwordCheck = password
+        for (index, _) in passwordCheck.enumerated() {
+            if index % 2 == 1 {
+                passwordCheck[index] = 0
+            }
+        }
+        passwordCheck = passwordCheck.filter({$0 != 0})
+        for num in passwordCheck { if num % 2 == 0 { return true } }
+        
+        return false
     }
     
 }
